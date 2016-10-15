08b8ddf21b0fcc63b76cf3f1d11ab73ef95f9487
Saharsh Hariharan <Saharsh Hariharan>
2016-10-15 12:11:26 -0400

    fdf

--- app/src/main/java/com/example/andig/patientwaittimes/FilterDateTime.java
+++ app/src/main/java/com/example/andig/patientwaittimes/FilterDateTime.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static java.lang.System.in;
 
 public class FilterDateTime extends AppCompatActivity {
 
     CalendarView calendar;
-    String time;
+    String Time;
     int Year;
     int Month;
     int Day;
@@ -21,7 +26,7 @@ public class FilterDateTime extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_date_time);
 
-        calendar = (CalendarView) findViewById (R.id.calendar);
+        calendar = (CalendarView) findViewById (R.id.ccalendar);
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
@@ -36,9 +41,15 @@ public class FilterDateTime extends AppCompatActivity {
             }
 
 
-        });
+        });}
+
+        private EditText time;
+        //public void onSubmit (View view) {
+
+           // time = (EditText) findViewById(R.id.editText);
+            //Time = time.getText().toString();
+           // System.out.println(Time);
+       // }
 
 
-
-    }
 }
