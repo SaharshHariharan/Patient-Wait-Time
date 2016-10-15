4443934d2b7016d846b027f13215c413e4776e30
Saharsh Hariharan <Saharsh Hariharan>
2016-10-15 05:30:44 -0400

    changes

--- app/src/main/java/com/example/andig/patientwaittimes/PatientsSchedule.java
+++ app/src/main/java/com/example/andig/patientwaittimes/PatientsSchedule.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class PatientsSchedule extends AppCompatActivity {
 
@@ -17,12 +18,15 @@ public class PatientsSchedule extends AppCompatActivity {
 
 
     calendar = (CalendarView) findViewById (R.id.calendar);
-    calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener()){
+    calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
+
+        @Override
+        public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+            Toast.makeText(PatientsSchedule.this, "hello", Toast.LENGTH_SHORT).show();
         }
-            @Override
-                    public void onSelectedDayChange(CalendarView view, int year, );
 
-        }
 
-}
+    }       );
+
+    }        }
